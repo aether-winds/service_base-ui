1bb3aac73bcb706b47174b84a9adde9ba475cb42
Patrick Ortiz <pogithub+pzychotis@gmail.com>
2025-08-21 23:44:40 -0400

    Add simple express app creation

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,14 +1,11 @@
-import {describe, it} from 'mocha';
 import {expect} from 'chai';
+import {describe, it} from 'mocha';
 import app from './index';
 
-describe('Express Application Tests', () => {
-  it('should receive a express application', () => {
-    // Verify that the exported app is an Express application
+describe('app', () => {
+  it('should be an express application', () => {
     expect(app).to.be.a('function');
     expect(app.listen).to.be.a('function');
     expect(app.use).to.be.a('function');
-    expect(app.get).to.be.a('function');
-    expect(app.post).to.be.a('function');
   });
 });
